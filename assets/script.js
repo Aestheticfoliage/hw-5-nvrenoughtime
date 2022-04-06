9d5817f15077c1e8b6e6b9948ecce9022a1b021d
dewey preast <webwalkin@me.com>
2022-04-05 21:16:57 -0400

    this adds the base skeleton of html and javascript

--- assets/script.js
+++ assets/script.js
@@ -3,23 +3,37 @@ const moment = require("moment")
 var project = $('#project')
 var type = $('#type')
 var dueDate = $('#dueDate')
-var perHour = $('#perHour')
+var perHour = $('#wage')
 var now = moment();
 var timer = $('');
 var inputData = $('');
-var submitEl = $("#button"
-)
+var submitEl = $("")
+var projectFormObj ={
+    project: document.querySelector("$project"),
+    type: document.querySelector("#type"),
+    dueDate: document.querySelector("#dueDate"),
+    perHour: document.querySelector("#wage"),
+    console.localStorage
 
+}
+var projectForm = document.querySelector(".projectForm" + project + type + wage + dueDate)
+projectForm.addEventListener("click", function(event(submitEl)) {
+    var element = event(submitEl).target;
+    projectFormObj(){
+        console.log()
+    }
+}
 var dt = new Date();
 document.getElementById('date-time').innerHTML=dt;
+$submitEl.addEventListener("click",function(inputData))
 
 var today = moment();
 $("#timer").text(today.format("MMM Do, YYYY"));
-var now = localStorage.getItem("now");
+var now = localStorage.getItem(today);
 function inputData (){
-    var perHour = JSON.parse(localStorage.getItem("perHour"));
-    var dueDate = JSON.parse(localStorage.getItem("dueDate"));
-    var type = JSON.parse(localStorage.getItem("type"))
+    var perHour = JSON.parse(localStorage.getItem(perHour));
+    var dueDate = JSON.parse(localStorage.getItem(dueDate));
+    var type = JSON.parse(localStorage.getItem(type))
     var project = JSON.parse(localStorage.getItem(project))
 localStorage.setItem("#perHour", JSON.stringify(perHour)
 );
@@ -31,8 +45,6 @@ return
 
 
 
-inputData(){
-    return
-}
+inputData();
 
 
