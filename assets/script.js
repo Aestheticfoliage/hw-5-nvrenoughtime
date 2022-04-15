6701151f6a40036c11b3de9249563d80f301ad89
dewey preast <webwalkin@me.com>
2022-04-15 15:41:08 -0400

    redoing the javascript to create elements

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,54 @@
-const moment = require("moment")
-
+@import "./moment"
 var project = $('#project')
 var type = $('#type')
 var dueDate = $('#dueDate')
 var perHour = $('#wage')
 var now = moment();
-var timer = $('');
-var inputData = $('');
+var timer = $("");
+var inputData = $("");
 var submitEl = $("")
 var projectFormObj ={
-    project: document.querySelector("$project"),
+    project: document.querySelector("#project"),
     type: document.querySelector("#type"),
     dueDate: document.querySelector("#dueDate"),
     perHour: document.querySelector("#wage"),
     console.localStorage
 
 }
-var projectForm = document.querySelector(".projectForm" + project + type + wage + dueDate)
-projectForm.addEventListener("click", function(event(submitEl)) {
-    var element = event(submitEl).target;
-    projectFormObj(){
-        console.log()
-    }
+function getData(data){
+    for (text.project($(project));
+    console.log(data);
+    data.text($(project) + dt)
+    text.type($(type) + wage);
+    console.log(data))
 }
+
+// var projectForm = document.querySelector(".projectForm" + project + type + wage + dueDate)
+// projectForm.addEventListener("click", function(event(submitEl)) {
+//     var element = event(submitEl).target;
+//     projectFormObj(){
+//         console.log()
+//     }
+// }
 var dt = new Date();
 document.getElementById('date-time').innerHTML=dt;
 $submitEl.addEventListener("click",function(inputData))
 
 var today = moment();
 $("#timer").text(today.format("MMM Do, YYYY"));
-var now = localStorage.getItem(today);
-function inputData (){
-    var perHour = JSON.parse(localStorage.getItem(perHour));
-    var dueDate = JSON.parse(localStorage.getItem(dueDate));
-    var type = JSON.parse(localStorage.getItem(type))
-    var project = JSON.parse(localStorage.getItem(project))
-localStorage.setItem("#perHour", JSON.stringify(perHour)
-);
-localStorage.setItem("#dueDate", JSON.stringify(dueDate));
-localStorage.setItem("#type",JSON.stringify(type));
-localStorage.setItem("#project",JSON.stringify(project));
-return
-}
+// var now = localStorage.getItem(today);
+// function inputData (){
+//     var perHour = JSON.parse(localStorage.getItem(perHour));
+//     var dueDate = JSON.parse(localStorage.getItem(dueDate));
+//     var type = JSON.parse(localStorage.getItem(type))
+//     var project = JSON.parse(localStorage.getItem(project))
+// localStorage.setItem("#perHour", JSON.stringify(perHour)
+// );
+// localStorage.setItem("#dueDate", JSON.stringify(dueDate));
+// localStorage.setItem("#type",JSON.stringify(type));
+// localStorage.setItem("#project",JSON.stringify(project));
+// return
+// }
 
 
 
