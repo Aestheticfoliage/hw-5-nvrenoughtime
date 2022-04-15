40dc0bcfb88f174addad09567f6de1ea4aa5822a
dewey preast <webwalkin@me.com>
2022-04-15 16:43:51 -0400

    working on creating elements

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,50 @@
-@import "./moment"
-var project = $('#project')
-var type = $('#type')
-var dueDate = $('#dueDate')
-var perHour = $('#wage')
+const moment = require("moment");
+
+var project = document.querySelector("project");
+var type = document.querySelector("type");
+var dueDate = document.querySelector("dueDate");
+var perHour = document.querySelector("wage");
 var now = moment();
-var timer = $("");
-var inputData = $("");
-var submitEl = $("")
+var timer = document.querySelector(setTimeout);
+var inputData = document.getElementsByClassName("form-control");
+var submitbuttonEl = document.addEventListener("click", SubmitEvent);
 var projectFormObj ={
     project: document.querySelector("#project"),
     type: document.querySelector("#type"),
     dueDate: document.querySelector("#dueDate"),
     perHour: document.querySelector("#wage"),
-    console.localStorage
 
 }
-function getData(data){
-    for (text.project($(project));
-    console.log(data);
-    data.text($(project) + dt)
-    text.type($(type) + wage);
-    console.log(data))
+function getData(){
+    project.textContent.concat(projectFormObj);
+    type.textContent.concat(projectFormObj);
+    dueDate.getAttribute.toString(projectFormObj);
+    perHour.getAttribute.toString(projectFormObj);
+    then (projectFormObj.SubmitEvent);
+    
+
+    console.log()
+    
 }
-
-// var projectForm = document.querySelector(".projectForm" + project + type + wage + dueDate)
-// projectForm.addEventListener("click", function(event(submitEl)) {
-//     var element = event(submitEl).target;
-//     projectFormObj(){
-//         console.log()
-//     }
-// }
-var dt = new Date();
-document.getElementById('date-time').innerHTML=dt;
-$submitEl.addEventListener("click",function(inputData))
-
-var today = moment();
-$("#timer").text(today.format("MMM Do, YYYY"));
-// var now = localStorage.getItem(today);
-// function inputData (){
-//     var perHour = JSON.parse(localStorage.getItem(perHour));
-//     var dueDate = JSON.parse(localStorage.getItem(dueDate));
-//     var type = JSON.parse(localStorage.getItem(type))
-//     var project = JSON.parse(localStorage.getItem(project))
-// localStorage.setItem("#perHour", JSON.stringify(perHour)
-// );
-// localStorage.setItem("#dueDate", JSON.stringify(dueDate));
-// localStorage.setItem("#type",JSON.stringify(type));
-// localStorage.setItem("#project",JSON.stringify(project));
-// return
-// }
+setTimeout(dueDate){
+    moment.calendarFormat.inputData.dueDate
+}
+ var setItem;
+  if (Array.projectFormObj.data)
+  { setItem}
+    submitbuttonEl.addEventListener("click")
+    data.projectFormObj.inputData();
+    dueDate.appendChild(data);
+    inputData.dueDate.appendChild();
+    
 
 
 
-inputData();
+
+function createDate(){
+
+        
+    };
+
 
 
