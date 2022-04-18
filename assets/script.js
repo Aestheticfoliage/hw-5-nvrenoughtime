26b30b9c48351f07b850d9cd2a9025b21930a4e7
dewey preast <webwalkin@me.com>
2022-04-18 05:12:29 -0400

    working on creating the projectForm once values are input

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,64 @@
-const moment = require("moment");
+const { unix } = require("./moment");
+
+moment = require("moment");
 
 var project = document.querySelector("project");
 var type = document.querySelector("type");
 var dueDate = document.querySelector("dueDate");
 var perHour = document.querySelector("wage");
-var now = moment();
-var timer = document.querySelector(setTimeout);
+var now = document.querySelector("date-time");
+var timer = document.querySelector("");
 var inputData = document.getElementsByClassName("form-control");
 var submitbuttonEl = document.addEventListener("click", SubmitEvent);
+// sets totalProjects to an array
+var totalProjects = [];
 var projectFormObj ={
     project: document.querySelector("#project"),
     type: document.querySelector("#type"),
     dueDate: document.querySelector("#dueDate"),
-    perHour: document.querySelector("#wage"),
-
+    perHour: document.querySelector("#wage")
 }
+// retrieving the data typed and assigning it to object projectForm
+
 function getData(){
-    project.textContent.concat(projectFormObj);
-    type.textContent.concat(projectFormObj);
+   for (projectFormObj.concat(type,dueDate,perHour,dueDate)
+    type.textContent.concat(projectFormObj)
     dueDate.getAttribute.toString(projectFormObj);
     perHour.getAttribute.toString(projectFormObj);
-    then (projectFormObj.SubmitEvent);
-    
+    projectFormObj.SubmitEvent;
+    projectFormObj = JSON.parse(projectFormObj)
+}
 
-    console.log()
-    
+// saving Data returned in the object projectForm to the name in each project
+function setData(){
+    localStorage.setItem("$(project)", projectFormObj)
+}
+// creating cards based on projectForm
+ function renderData(projectFormobj){
+
+ }
+function SubmitEvent(createDate){
+    array.forEach.projectFormObj => {
+        
+    });
 }
 setTimeout(dueDate){
     moment.calendarFormat.inputData.dueDate
 }
- var setItem;
-  if (Array.projectFormObj.data)
-  { setItem}
-    submitbuttonEl.addEventListener("click")
-    data.projectFormObj.inputData();
-    dueDate.appendChild(data);
-    inputData.dueDate.appendChild();
+//  var setItem;
+//   if (Array.projectFormObj.data)
+//   { setItem}
+//     submitbuttonEl.addEventListener("click")
+//     data.projectFormObj.inputData();
+//     dueDate.appendChild(data);
+//     inputData.dueDate.appendChild();
     
 
 
 
 
 function createDate(){
+    FormData.apply
 
         
     };
