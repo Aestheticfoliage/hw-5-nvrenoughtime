74dc185af95fe8acd5676bbc044d2a25238e118f
dewey preast <webwalkin@me.com>
2022-04-27 21:01:59 -0400

    working to fix functionality and make look correct

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-// moment = require("moment");
-// var now = moment();
+var now = moment().format("HHMM");
+console.log(now);
 var projectFormEl = $("#projectForm");
 var projectEl = $("#project");
 var typeEl = $("#type");
@@ -7,19 +7,25 @@ var dueDate = $("#dueDate");
 var perHour = $("#perHour");
 var userInput = $("#userInput");
 // var submitbuttonEl = $(handleFormSubmit);
+/*{$(".container").on("click", ".saveBtn", 
+function (event) {
+    var target = $(event.currentTarget);
+  //[12:00 PM]
+  
+return;
+}
 function handleFormSubmit(event) {
   event.preventDefault();
-  var projectFormEl = $('input[name= "projectForm"]').val();
+  var projectFormEl = $(`input[name= "projectForm"]`).val();
   if (!projectFormEl) {
     console.log("no projects today");
-    return;
-  }
+    
   projectFormEl.append("<form>" + projectFormObj + "</form>");
-  $('input[name= "projectForm"]').val("");
+  $(`input[name= "projectForm"]`).val("");
   var test3 = $(`<div class="col-md-6">test3</div>`);
   userInput.append(test3);
-  for (var i = 0; i < totalProjects.length; i++) {}
-}
+  for (var i = 0; i < totalProjects.length; i++) {}}
+
 userInput.css("border", "2px solid red");
 // userInput.html(`<div class="col-md-6">
 // test
@@ -55,3 +61,26 @@ function renderData(projectFormobj) {}
 //     data.projectFormObj.inputData();
 //     dueDate.appendChild(data);
 //     inputData.dueDate.appendChild()
+*/
+console.log("Current time", moment().hours());
+
+$(".saveBtn").on("click", function () {
+  console.log("Save button clicked", $(this));
+  //grab the time label
+  var time = $(this).siblings(".hour").text();
+  console.log(time);
+  var description = $(this).siblings(".description").val(); //value
+  var wage = $(this).siblings(".wage").val(); //value
+  var dueDate = $(this).siblings(".project-date").val(); //value
+
+  var projectDetails = {
+    description: description,
+    hourlyWage: wage,
+    projectDate: dueDate,
+  };
+  console.log(projectDetails);
+
+  //save it to local storage
+  //localStorage.setItem(time, description);
+  localStorage.setItem(time, JSON.stringify(projectDetails));
+});
